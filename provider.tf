6e259a382b0fede743cd2611d321fa87ab294616
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-05-01 18:08:20 +0530

    Terraform VPC

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-86s-remote" 
+    key = "vpc-module-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
